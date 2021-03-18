c53dc3bc6677a59b0f2aefeb72303005b437f5bd
ar1a17 <78399657+ar1a17@users.noreply.github.com>
2021-03-18 17:45:18 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,97 @@
+             ################ data structure ################
+#list
+#data=[1,1,2,5,3,'ajay','india','python']
+"""b=[2,'ashutosh']
+data.append(b)
+data.remove(b)"""
+"""data.insert(2,'ashutosh')
+data.remove('ashutosh')"""
+"""data.remove(b)"""
+"""data.extend(b)"""
+#data.append ('rawat')
+#data.insert(2,1565)
+#data.count(1)
+#data.remove(5)
+#data.reverse()
+#del data[-1]
+#print(data)
+"""Using lists as stack and queue"""
+"""a=[1,23,60]
+a.pop(0)
+print(a)
+"""
+#A few special functions related to lists
+"""value=[1,3,5,9,2.2]
+print(all(value))"""
+"""c=[56,555,858,99]
+value.extend(c)
+print(max(value))
+print(min(value))"""
+#tuples
+"""a='ajay','is','a','boy'
+for v in a:
+    print(v,end ='')"""
+"""a=(11,22,525)
+del a[0]
+print(a)"""
+#check the type
+"""a={'ajay':'ashutosh'}
+print(type(a))"""
+#SETS
+"""a=set('abcdef')
+#a.add('p')
+print(a)"""
+#Dictionaries
+"""a={'ajay':25,'ashutosh':26}
+#del a['ajay']
+#print('raj' in a)
+print(a)"""
+"""
+data={}
+data.setdefault('name',[]).append('raj')
+print(data"""
+"""data={'ajay':'boy','manisha':'girl'}"""
+"""for x in data:
+    print(f"Key = {x}")"""
+
+"""a='ajay'
+print(len(a))"""
+#string
+"""s="Ar1a1a7"
+print(s.isalnum())"""
+
+"""s="nature is good"
+print(s.find("o"))
+"""
+#functions
+"""def sum(b,c):
+    return b-c
+rea=sum(45,45)
+print(rea)"""
+
+"""def palindrome(s):
+    return s == s[::-1]
+
+if __name__ == "__main__":
+    s = input("Enter a string: ")
+    if palindrome(s):
+        print("Yay a palindrome")
+    else:
+        print("Oh no, not a palindrome")"""
+
+"""#local variable
+def change(a):
+    a = 90
+    print(f"Inside of the change function {a}")
+a = 9
+print(f"Before the function call {a}")
+change(a)
+print(f"After the function call {a}")
+"""
+
+
+"""def test(a,b=-99):
+    if a>b:
+        return true
+    else:
+        return fals"""
---
+++ day1.py
@@ -0,0 +1,112 @@
+#a=10+52
+#print(a)
+
+#a=10<55
+#print(a)
+#name="ajay"
+#print(name)
+
+#how to get user input
+#number=int(input("enter the digit:"))
+#if number<100:
+#     print("your number smaller than 100")
+#else:
+#     print("your number greater than 100")
+#a ,v    = 5 , 6
+#print(v)
+
+#data=("ajay",25,"male")
+#name,age,sex=data
+#print(age)
+# Formatting strings
+#marks=45
+#marks=56
+#print(f"the marks is {marks}")
+
+#name= "ajay"
+#age=25
+#print(f"the is name {name} and the age is {age}")
+
+
+#.Formatting
+#name="ajay"
+#age=25
+#user="{0}{1}" .format(name,age)
+#print(user)
+
+#integer arithmetic
+#days=int(input("enter the days:"))
+#months = days/30
+#days = days % 30
+#print("months = %d days = %d" % (months,days))
+
+# 2 method easy way to use interger arithmetic
+#days=int(input("Enter the days :"))
+#print("months=%d,days=%d"%(divmod(days,30)))
+
+#Relational Operator
+#"a=2.5
+#"str(a)
+#print(a)
+
+#if else statement
+#x=int(input("enter the value: "))
+#if x>0:
+#    x=0
+#    print('negitive change the zero')
+#elif x==0:
+#    print("zero")
+#elif x==1:
+#    print('single')
+#else:
+#    print('more')
+#while loop
+#x=0
+#while x<11:
+#    print(x)
+#    x+=1
+#a,b=0,1
+#while b<100:
+#    print(b)
+#    a,b=b, a+b
+#multiplication table
+#i = 1
+#print("-" * 50)
+#while i < 11:
+#    n = 1
+#    while n <= 12:
+#        print("%4d" % (i * n), end=' ')
+#        n += 1
+#    print()
+#    i += 1
+#print("-" * 50)
+
+#printing star
+#row=int(input("enter the number of row:"))
+#n=row
+#while n>=1:
+#    x="*" * n
+#    print(x)
+#    n-=1
+#list
+"""a=[1,2,'ajay','india']
+print(a[1:2])
+"""
+"""str="hello this is ajay"
+print(len(str))
+print(str[0:7])"""
+#for loop
+"""for i in range(0,10):
+    print(i)
+else:
+    print("bye")"""
+a=['ajay','ashutosh']
+for c in a:
+    print(c)
+#append
+"""a=[2,65,6,4]
+a.append(45)
+print(a)
+"""
+"""data={'ajay':'','ashutosh','anil'}
+print(data)"""
