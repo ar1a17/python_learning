3ea7c55e2f035a5c107ed1fd94835b5e40ddeea2
Ajay Rawat <ajayrawat707@gmail.com>
2021-03-18 11:56:35 +0530

    first commit on github

---
+++ day3.py
@@ -0,0 +1,21 @@
+"""#how to create function
+def my_function():
+    a=5
+    b=6
+    c=a+b
+    print(c)
+#function calling..
+my_function()"""
+
+
+# one Argument
+"""def my_function(fname):
+    print(fname+6)
+
+my_function(4)
+my_function(6)
+"""
+# on or more Argument
+def my_function(fname,lname,vname):
+    print(fname + " " + lname + vname)
+my_function("ajay")
