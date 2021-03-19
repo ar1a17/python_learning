ef5ba68bf7e4be141b6d64a9ef5f6c09fe4f741c
Ajay Rawat <ajayrawat707@gmail.com>
2021-03-19 11:26:43 +0530

    explore more about class

---
+++ day4.py
@@ -0,0 +1,14 @@
+#####class and object#######
+"""class MyClass:
+  x = 5
+  y=5
+  z=x+y
+p1 = MyClass()#creating object
+print(p1.z)"""
+
+class Ajay:
+    x=5
+    y=4
+    z=x>y
+a1=Ajay()
+print(a1.z)
