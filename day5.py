2a8ed2a109d4a98d22bc1d1014bf5673641fb07a
Ajay Rawat <ajayrawat707@gmail.com>
2021-03-23 16:55:58 +0530

    python inheritance

--- day5.py
+++ day5.py
@@ -1,4 +1,32 @@
-class x:
-    print("hii")
-p1=x
-print(p1)
+"""class human:
+  def __init__(person, name, age):
+    person.name = name
+    person.age = age
+
+p1 = human("ajay", 23)
+print(p1.name)
+print(p1.age)
+"""
+#object method
+"""class human:
+  def __init__(person, name, age):
+    person.name = name
+    person.age = age
+
+  def myfunc(person):
+    print("Hello my name is " + person.name , "and my age is " + str(person.age))
+p1 = human("ajay", 23)
+p1.myfunc()"""
+
+
+"class Person:"
+
+""" def __init__(Data, name, age):
+    Data.name = name
+    Data.age = age
+
+  def myfunc(d):
+    print("Hello my name is " + d.name)
+
+p1 = Person("ajay", 23)
+p1.myfunc()"""
