ea90296aa0376a5050f814ed228696d5f2a6c506
Ajay Rawat <ajayrawat707@gmail.com>
2021-04-03 16:44:31 +0530

    python packages

---
+++ day9.py
@@ -0,0 +1,19 @@
+
+#Convert from JSON to Python:
+"""import json
+x='{"name":"ajay","age":32,"city":"new delhi"}'
+y=json.loads(x)
+print(y["age"])"""
+
+#Convert from Python to JSON:
+"""import json
+x='{name:ajay,city:delhi,age:30}'
+y=json.dumps(x)
+print(y)"""
+
+
+from tkinter import *
+
+top = Tk()
+
+top.mainloop()
